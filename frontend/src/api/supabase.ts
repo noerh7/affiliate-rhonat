dc3661a96beae810f611297cef4db277b8e3ca96
spacemen100 <happenparis@protonmail.com>
2025-12-03 11:25:27 +0400

    hhjh

--- frontend/src/api/supabase.ts
+++ frontend/src/api/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const url = import.meta.env.VITE_SUPABASE_URL || 'https://etkeimmyqfangzyrajqx.supabase.co';
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0a2VpbW15cWZhbmd6eXJhanF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MDgxNjksImV4cCI6MjA4MDE4NDE2OX0.TF4TBcnrbNVLdZSQEnrOE6xCSE1KGHd4WGX-1rGoBdc';
+const url = import.meta.env.VITE_SUPABASE_URL || 'https://ionoburxknruxedgivno.supabase.co';
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlvbm9idXJ4a25ydXhlZGdpdm5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2OTY3MzQsImV4cCI6MjA4MDI3MjczNH0.yu72hL7wURNg62gKHSVehTlCIq637wjrmzGd5sv76MY';
 
 export const supabase = createClient(url, key);
